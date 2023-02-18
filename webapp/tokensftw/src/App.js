c7f2894073403eedc60e602ba7eb1628904a4ba9
Aryan Kharbanda <kharbanda.aryan00@gmail.com>
2023-02-18 17:41:57 +0530

    arcanized the dapp

--- webapp/tokensftw/src/App.js
+++ webapp/tokensftw/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import './App.css';
+// arcana
+import { Auth, useAuth } from "@arcana/auth-react";
 
 import Navbar from './components/js/navbar';
 import Login from './components/js/login';
@@ -13,37 +15,57 @@ import StakeHolderDashboard from './components/js/stakeHolderDashboard';
 import TenantDashboard from './components/js/tenantDashboard';
 import PropertyRent from './components/js/propertyRent';
 
+// arcana
+const onLogin = () => {
+    // Route to authenticated page
+
+}
+
 const Layout = () => {
-  return (
-    <div>
-      <Navbar />
-      <div className="container">
-        <Outlet />
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <Navbar />
+            <div className="container">
+                <Outlet />
+            </div>
+        </div>
+    );
 };
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Login />} />
-            <Route path="owner-dashboard" element={<OwnerDashboard />} />
-            <Route path="stakeholder-dashboard" element={<StakeHolderDashboard />} />
-            <Route path="tenant-dashboard" element={<TenantDashboard />} />
-            <Route path="initialize-nft" element={<InitializeNFT />} />
-            <Route path="buy-stake" element={<BuyStake />} />
-            <Route path="sell" element={<SellStake />} />
-            <Route path="tenants" element={<Tenants />} />
-            <Route path="property-rent" element={<PropertyRent />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    const auth = useAuth();
+    return (
+        <div>
+            <div>
+                {auth.loading ? (
+                    "Loading"
+                ) : auth.isLoggedIn ? (
+                    <p>Logged In</p>
+                ) : (
+                    <div>
+                        <Auth externalWallet={true} theme={"light"} onLogin={onLogin} />
+                    </div>
+                )}
+            </div>
+            <div className="App">
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Layout />}>
+                            <Route index element={<Login />} />
+                            <Route path="owner-dashboard" element={<OwnerDashboard />} />
+                            <Route path="stakeholder-dashboard" element={<StakeHolderDashboard />} />
+                            <Route path="tenant-dashboard" element={<TenantDashboard />} />
+                            <Route path="initialize-nft" element={<InitializeNFT />} />
+                            <Route path="buy-stake" element={<BuyStake />} />
+                            <Route path="sell" element={<SellStake />} />
+                            <Route path="tenants" element={<Tenants />} />
+                            <Route path="property-rent" element={<PropertyRent />} />
+                        </Route>
+                    </Routes>
+                </BrowserRouter>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- webapp/tokensftw/src/index.js
+++ webapp/tokensftw/src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// ARCANA
+import { AuthProvider } from "@arcana/auth";
+import { ProvideAuth } from "@arcana/auth-react";
+console.log(process.env.REACT_APP_ARCANA_APP_ID)
+const provider = new AuthProvider(process.env.REACT_APP_ARCANA_APP_ID) // App address
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <ProvideAuth provider={provider}>
+            <App />
+        </ProvideAuth>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
